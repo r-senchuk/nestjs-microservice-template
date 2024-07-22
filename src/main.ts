81d5ff0f0cfbc87cc0c27c811b947c58afe059e9
Roman Yakoviv <ryakoviv@vodworks.com>
2024-07-22 17:04:51 +0300

    clusterize application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { configureApp } from './main.config';
+import cluster from 'cluster';
+import os from 'os';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -15,4 +17,22 @@ async function bootstrap() {
   configureApp(app);
   await app.listen(process.env.HTTP_PORT as string, '0.0.0.0');
 }
-bootstrap();
+
+function clusterize(callback: () => void): void {
+  const numCPUs = os.cpus().length;
+  console.log(numCPUs);
+  if (cluster.isPrimary) {
+    console.log(`MASTER SERVER (${process.pid}) IS RUNNING `);
+
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+      console.log(`worker ${worker.process.pid} died`);
+    });
+  } else {
+    callback();
+  }
+}
+clusterize(bootstrap);
